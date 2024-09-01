a4f1c30ab577f17625c62b0ddb5950dc82499b89
Islan <islan12victhor@gmail.com>
2024-09-01 17:06:24 -0300

    feat: initial product and product_image models
    - "Created" the initial model for these two items

--- backend/products/models/product_image.py
+++ backend/products/models/product_image.py
@@ -1,1 +1,8 @@
 from django.db import models
+from products.models.product import Product
+
+class ProductImage(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    alt_text = models.CharField(max_length=50)
+    # image = models.CharField(max_length=255, blank=True, null=True)
