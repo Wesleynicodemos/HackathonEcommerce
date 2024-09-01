24437c798840e1dd3ae9d62b5cf778ca39aea742
Islan <islan12victhor@gmail.com>
2024-09-01 17:50:23 -0300

    feat: initial orders models
    - "Created" the initial models for order, order_item and payment

--- backend/orders/models/order.py
+++ backend/orders/models/order.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class Order(models.Model):
+    # user_id = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    shipping_address = models.CharField(max_length=255)
+    status = models.CharField(max_length=20 , choices=[('pending', 'Pending'), ('processing', 'Processing'), ('shipped', 'Shipped'), ('delivered', 'Delivered')], default='pending')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        # ordering = []
+        pass
+    
+    def __str__(self):
+        pass
