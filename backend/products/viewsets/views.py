b1bf34d0c05e5d0ebf14db661f4694ad6583367f
Islan <islan12victhor@gmail.com>
2024-09-02 22:52:55 -0300

    feat: Updated products views
    - Updated the products views. Now it does what it needs to.

--- backend/products/viewsets/views.py
+++ backend/products/viewsets/views.py
@@ -2,12 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
+from products.models.product import Product
+from products.models.product_image import ProductImage
+from products.serializers.product_serializer import ProductSerializer
+from products.serializers.product_image_serializer import ProductImageSerializer
+
 class ProductsList(APIView):
     def get(self, request):
-        data = {'Message': "Hello There! Products List"}
-        return Response(data)
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
 
 class ProductsDetails(APIView):
     def get(self, request, product_id):
-        data = {'Message': 'Hello There! Products Details', 'Product': product_id}
-        return Response(data)
+        product = get_object_or_404(Product, pk=product_id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
